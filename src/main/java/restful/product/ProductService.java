fe584747f63cbd3e8c9069c2fe1496f858f477f2
utkir1212 <usoporboev3@gmail.com>
2020-03-27 15:27:11 +0500

    create project

---
+++ src/main/java/restful/product/ProductService.java
@@ -0,0 +1,30 @@
+package restful.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product save(Product stock) {
+        return productRepository.save(stock);
+    }
+
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
