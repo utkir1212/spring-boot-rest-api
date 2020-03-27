280bfbffba95a76f23698ddf416ab397308625be
utkir1212 <usoporboev3@gmail.com>
2020-03-27 15:31:02 +0500

    add test for product model

---
+++ src/test/java/restful/product/ProductTest.java
@@ -0,0 +1,26 @@
+package restful.product;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+
+    @Test
+    void test_createProduct() {
+
+        assertTrue(true);
+    }
+
+}
